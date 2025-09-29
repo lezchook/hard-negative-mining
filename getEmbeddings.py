55b954aa8232ef167f6640217411a79639418b9b
Sergei Leshchenko <leshchsergei@gmail.com>
2025-09-30 00:03:18 +0300

    add getEmbeddings

---
+++ getEmbeddings.py
@@ -0,0 +1,82 @@
+import torch
+from torch import Tensor
+from transformers import AutoTokenizer, AutoModel
+from datasets import load_dataset
+import torch.nn.functional as F
+import argparse
+from tqdm.auto import tqdm
+import json
+import random
+import numpy as np
+import os
+
+def set_seed(seed: int = 42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+
+def last_token_pool(last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
+    left_padding = (attention_mask[:, -1].sum() == attention_mask.shape[0])
+    if left_padding:
+        return last_hidden_states[:, -1]
+    else:
+        sequence_lengths = attention_mask.sum(dim=1) - 1
+        batch_size = last_hidden_states.shape[0]
+        return last_hidden_states[torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths]
+
+def get_detailed_instruct(task_description: str, query: str) -> str:
+    return f'Instruct: {task_description}\nQuery: {query}'
+
+def encode_texts(model: AutoModel, tokenizer: AutoTokenizer, input_text: str, device: str = "cuda") -> Tensor:
+    texts_dict = tokenizer(input_text, max_length=4096, padding=True, truncation=True, return_tensors='pt').to(device)
+    with torch.no_grad():
+        outputs = model(**texts_dict)
+    embeddings = last_token_pool(outputs.last_hidden_state, texts_dict['attention_mask'])
+    embeddings = F.normalize(embeddings, p=2, dim=1)
+    return embeddings
+
+def start_inference(data_path, output_file):
+    task = 'Given a web search query, retrieve relevant passages that answer the query'
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    data = load_dataset(data_path)
+    tokenizer = AutoTokenizer.from_pretrained("intfloat/e5-mistral-7b-instruct")
+    model = AutoModel.from_pretrained("intfloat/e5-mistral-7b-instruct", device_map="auto") # .to(device)
+    model.eval()
+    
+    with open(output_file, "a", encoding="utf-8") as f:
+        pbar = tqdm(total=len(data["train"]), desc="Processing", unit="item")
+
+        for idx, sample in enumerate(data["train"]):
+            query = sample["question"]
+            query_text = get_detailed_instruct(task, query)
+            context = sample["context"]
+            query_emb = encode_texts(model, tokenizer, query_text, device)
+            context_emb = encode_texts(model, tokenizer, context, device)
+
+            result = {
+                "index": idx,
+                "query": query,
+                "context": context,
+                "query_emb": query_emb.cpu().tolist(),
+                "context_emb": context_emb.cpu().tolist()
+            }
+
+            f.write(json.dumps(result, ensure_ascii=False) + "\n")
+            f.flush()
+            pbar.update(1)
+
+        pbar.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str, required=True)
+    parser.add_argument("--output_file", type=str, required=True)
+    args = parser.parse_args()
+
+    set_seed(42)
+    start_inference(args.data_path, args.output_file)
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py --data_path kuznetsoffandrey/sberquad --batch_size 128 --output_file /app/hardneg_dataset.jsonl
+python getEmbeddings.py --data_path kuznetsoffandrey/sberquad --output_file /app/embeddings_dataset.jsonl
